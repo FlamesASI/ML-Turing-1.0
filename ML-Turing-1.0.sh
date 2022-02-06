6513c4041bddcb4c7596f05a97aeec60149aa26c
Ell Citron <98425920+FlamesASI@users.noreply.github.com>
2022-02-06 09:23:11 -0800

    Request to upload 2.6.22
    Time stamp 9:23 AM FST

---
+++ ML-Turing-1.0.sh
@@ -0,0 +1,43 @@
+## write the code for managing a juypter notebook in  your vs code for using any gpt3 prompt
+
+print("Welcome ML-Turing 1.0")
+print("This is a simple script to manage your jupyter notebook in your vs code")
+print("This script is for using gpt3 prompt")
+## write the script
+print("Please enter the name of your notebook")
+notebook_name = input()
+print("Please enter the name of your notebook")
+## make a markov algrothim to self make new notebooks under your name in python
+## and generate new code
+print("Please enter your google colab url")
+colab_url = input()
+print("Please enter your google colab password")
+colab_password = input()
+print("Please enter your google colab jupyter notebook url")
+jupyter_url = input()
+## write the code for managing a juypter notebook in  your vs code for using any gpt3 prompt
+os.system("jupyter notebook --generate-config")
+## make a self writing program
+os.system("echo \"c.NotebookApp.ip = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.password = u''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.notebook_dir = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.open_browser = False\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.port = 8888\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.token = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_origin = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_credentials = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_root = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_remote_access = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_websocket_origin = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+## write the code for managing a juypter notebook in  your vs code for using any gpt3 prompt
+os.system("echo \"c.NotebookApp.ip = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.password = u''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.notebook_dir = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.open_browser = False\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.port = 8888\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.token = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_origin = ''\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_credentials = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_root = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_remote_access = True\" >> ~/.jupyter/jupyter_notebook_config.py")
+os.system("echo \"c.NotebookApp.allow_
